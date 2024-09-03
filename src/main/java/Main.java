39ceeea19a18a447e14b3730eff4ef3682e5943e
murhaf2211 <orfali.murhaf@web.de>
2024-09-03 11:27:33 +0200

    Add numbers and print the sum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hello World!");
         String title = " this is the new Title!";
         System.out.println(title);
+        int a = 7;
+        int b = 66;
+        System.out.println(a + b);
     }
 }
